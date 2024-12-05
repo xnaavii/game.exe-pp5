9824a85216e296aec1b414d27283464c66abc364
Ivan <petrovic.valpovo@gmail.com>
2024-12-05 21:34:40 +0000

    Create game model

--- discover/models.py
+++ discover/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+class Game(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    genre = models.CharField(max_length=100)
+    platform = models.CharField(max_length=100)
+    release_date = models.DateField()
+    rating = models.FloatField(default=0.0)
+
+    def __str__(self):
+        return self.title
